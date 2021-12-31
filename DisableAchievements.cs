d4ac6743fd0870087f5dd6e39d2bed2fe219a8fa
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2021-12-30 23:48:26 -0500

    Disable achievements and leaderboard submissions

--- DisableAchievements.cs
+++ DisableAchievements.cs
@@ -1,3 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+
 namespace XRL.DisableAchievements
 {
+    [HarmonyPatch(typeof(LeaderboardManager), "SubmitResult")]
+    public static class PatchSubmitResult
+    {
+        public static bool Prefix(ref string __result)
+        {
+            // skip submission to the leaderboard
+            __result = null;
+            return false;
+        }
+    }
+
+    [HarmonyPatch]
+    public static class PatchAchievementManager
+    {
+        public static IEnumerable<MethodBase> TargetMethods()
+        {
+            return AccessTools.TypeByName("AchievementManager")
+                .GetMethods()
+                .Where(method => !method.IsConstructor && method.ReturnType == typeof(void))
+                .Cast<MethodBase>();
+        }
+
+        public static bool Prefix()
+        {
+            // skip all achievement manager stuff
+            return false;
+        }
+    }
+
 }
